9ea4520def5c2c6a9eb99eedb1ed20b4d28acaba
EgnuernaSorin <esorinfrus@gmail.com>
2026-09-10 18:30:16 -0700

    Happy birthday command
    happy birthday

--- src/main/java/frc/robot/commands/HappyBirthdayCommand.java
+++ src/main/java/frc/robot/commands/HappyBirthdayCommand.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class HappyBirthdayCommand extends Command {
   // Times this method was ran:
   // 3/24/2026: Nora's Birthday YEEEPEEEE
-  TalonFX motor = new TalonFX(1); // your CAN ID
-
+  // 9/3/2026: Nolan's birthday
   Orchestra orchestra = new Orchestra();
 
   public HappyBirthdayCommand() {
-    orchestra.addInstrument(motor);
+    for(int i = 1; i < 100; i++){
+      TalonFX motor = new TalonFX(i);
+      orchestra.addInstrument(motor);
+    }
   }
 
   @Override
