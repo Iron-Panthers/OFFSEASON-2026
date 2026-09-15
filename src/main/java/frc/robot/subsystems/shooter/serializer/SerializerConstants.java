7239502aca920ac0fec0a554641755cc071ba134
Taku <takuwonoe@gmail.com>
2026-09-14 18:26:19 -0700

    Retuned Seralizer
    Should be good

--- src/main/java/frc/robot/subsystems/shooter/serializer/SerializerConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/serializer/SerializerConstants.java
@@ -9,9 +9,9 @@ public class SerializerConstants {
         case SIM -> new SerializerConfig(
             CAN.at(32, "Serializer"), CAN.at(0, "Serializer 2"), 5, true, false, true);
         case COMP -> new SerializerConfig(
-            CAN.at(41, "Serializer Left"),
-            CAN.at(33, "Serializer Right"),
-            2.833333,
+            CAN.at(33, "Serializer Left"),
+            CAN.at(41, "Serializer Right"),
+            1.647,
             false,
             false,
             true);
@@ -22,7 +22,7 @@ public class SerializerConstants {
   public static final PIDGains GAINS =
       switch (Constants.getRobotType()) {
         case SIM -> new PIDGains(1, 0, 0, 0, 1, 0, 0);
-        case COMP -> new PIDGains(0.5, 0, 0, 0.2, 0.344827586, 0, 0);
+        case COMP -> new PIDGains(0.5, 0, 0, 0.32, 0.192, 0.051, 0);
         default -> new PIDGains(0, 0, 0, 0, 0, 0, 0);
       };
 
