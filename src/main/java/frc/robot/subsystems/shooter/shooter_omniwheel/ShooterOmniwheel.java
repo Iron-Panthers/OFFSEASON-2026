66e63a1ee6ee019f5ce8e762f4204e7485efa6e3
EgnuernaSorin <esorinfrus@gmail.com>
2026-09-19 08:29:15 -0700

    Qual 5

--- src/main/java/frc/robot/subsystems/shooter/shooter_omniwheel/ShooterOmniwheel.java
+++ src/main/java/frc/robot/subsystems/shooter/shooter_omniwheel/ShooterOmniwheel.java
@@ -6,7 +6,7 @@ import frc.robot.lib.generic_subsystems.rollers.*;
 
 public class ShooterOmniwheel extends GenericRollers<ShooterOmniwheel.ShooterOmniwheelTarget> {
   public enum ShooterOmniwheelTarget implements GenericRollers.VelocityTarget {
-    IDLE(-25, ShooterOmniwheelConstants.CURRENT_LIMIT_AMPS),
+    IDLE(-16.7, ShooterOmniwheelConstants.CURRENT_LIMIT_AMPS),
     OFF(-1, ShooterOmniwheelConstants.CURRENT_LIMIT_AMPS),
     SHOOT(90, ShooterOmniwheelConstants.CURRENT_LIMIT_AMPS);
 
