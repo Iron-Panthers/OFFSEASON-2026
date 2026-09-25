111b8c0690d028d2e1dc6e4b2236d8572eb07738
Taku <takuwonoe@gmail.com>
2026-09-24 18:16:25 -0700

    removed unnecessary stuff
    dont worry about it

--- src/main/java/frc/robot/commands/SmartIntakeCommand.java
+++ src/main/java/frc/robot/commands/SmartIntakeCommand.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.elastic_updater.ElasticUpdater;
 import frc.robot.subsystems.intake.IntakeController;
@@ -83,10 +82,5 @@ public class SmartIntakeCommand {
                                                 (shooterController.getTargetState()
                                                     == ShooterState.TOTAL_SPIN_UP))))
                                 .repeatedly()));
-  }
-
-  private void alongWith(ParallelCommandGroup alongWith) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'alongWith'");
   }
 }
